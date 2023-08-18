0060ae55321187d0f7e06c318708c5dc1a2bfc76
felipesanderp <felipe.panisson@hotmail.com>
2023-08-18 10:51:47 -0300

    form validations

--- src/components/checkout-form.tsx
+++ src/components/checkout-form.tsx
@@ -17,13 +17,16 @@ import { useRouter } from 'next/navigation'
 import { useCartStore } from '@/stores/useCartStore'
 
 const FormSchema = z.object({
-  cep: z.string(),
-  street: z.string(),
-  number: z.string(),
+  cep: z.string().min(8, { message: 'O CEP deve conter 8 caracteres' }),
+  street: z.string().nonempty({ message: 'A rua é obrigatória!' }),
+  number: z.string().nonempty({ message: 'Número da casa é obrigatório!' }),
   complement: z.string().optional(),
-  neighborhood: z.string(),
-  city: z.string(),
-  uf: z.string().max(2),
+  neighborhood: z.string().nonempty({ message: 'O bairro é obrigatório!' }),
+  city: z.string().nonempty({ message: 'A cidade é obrigatória!' }),
+  uf: z
+    .string()
+    .min(2, { message: 'Mínimo 2!' })
+    .max(2, { message: 'Máximo 2!' }),
   payment_type: z.enum(['credit_card', 'debit_card', 'money']),
 })
 
@@ -63,7 +66,7 @@ export function CheckoutForm() {
         className="flex flex-col gap-3"
         id="checkout-form"
       >
-        <div className="flex h-[23.25rem] flex-col gap-8 rounded-md bg-zinc-200 p-10">
+        <div className="flex h-auto flex-col gap-8 rounded-md bg-zinc-200 p-10">
           <div className="flex gap-2">
             <Icons.mapPin className="stroke-yellow-800" size={22} />
             <div>
@@ -75,49 +78,10 @@ export function CheckoutForm() {
           </div>
 
           <div className="flex flex-col gap-4">
-            <FormField
-              control={form.control}
-              name="cep"
-              render={({ field }) => (
-                <FormItem className="relative">
-                  <FormControl>
-                    <Input
-                      type="text"
-                      className="peer block w-52"
-                      placeholder=" "
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormLabel className="absolute left-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-zinc-300 text-sm text-zinc-600 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-yellow-800">
-                    CEP
-                  </FormLabel>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="street"
-              render={({ field }) => (
-                <FormItem className="relative">
-                  <FormControl>
-                    <Input
-                      type="text"
-                      className="peer block"
-                      placeholder=" "
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormLabel className="absolute left-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-zinc-300 text-sm text-zinc-600 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-yellow-800">
-                    Rua
-                  </FormLabel>
-                </FormItem>
-              )}
-            />
-
-            <div className="flex items-center gap-3">
+            <div className="flex flex-col gap-2">
               <FormField
                 control={form.control}
-                name="number"
+                name="cep"
                 render={({ field }) => (
                   <FormItem className="relative">
                     <FormControl>
@@ -129,58 +93,24 @@ export function CheckoutForm() {
                       />
                     </FormControl>
                     <FormLabel className="absolute left-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-zinc-300 text-sm text-zinc-600 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-yellow-800">
-                      Número
+                      CEP
                     </FormLabel>
                   </FormItem>
                 )}
               />
-              <FormField
-                control={form.control}
-                name="complement"
-                render={({ field }) => (
-                  <FormItem className="relative w-full">
-                    <FormControl>
-                      <Input
-                        type="text"
-                        className="peer block placeholder:text-xs placeholder:italic"
-                        placeholder="Opcional"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormLabel className="absolute left-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-zinc-300 text-sm text-zinc-600 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-yellow-800">
-                      Complemento
-                    </FormLabel>
-                  </FormItem>
-                )}
-              />
+              {form.formState.errors.cep && (
+                <span className="text-[0.8rem] font-medium text-red-500">
+                  {form.formState.errors.cep.message}
+                </span>
+              )}
             </div>
 
-            <div className="flex items-center gap-3">
+            <div className="flex flex-col gap-2">
               <FormField
                 control={form.control}
-                name="neighborhood"
+                name="street"
                 render={({ field }) => (
                   <FormItem className="relative">
-                    <FormControl>
-                      <Input
-                        type="text"
-                        className="peer block w-52"
-                        placeholder=" "
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormLabel className="absolute left-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-zinc-300 text-sm text-zinc-600 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-yellow-800">
-                      Bairro
-                    </FormLabel>
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="city"
-                render={({ field }) => (
-                  <FormItem className="relative w-full">
                     <FormControl>
                       <Input
                         type="text"
@@ -190,30 +120,151 @@ export function CheckoutForm() {
                       />
                     </FormControl>
                     <FormLabel className="absolute left-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-zinc-300 text-sm text-zinc-600 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-yellow-800">
-                      Cidade
+                      Rua
                     </FormLabel>
                   </FormItem>
                 )}
               />
-              <FormField
-                control={form.control}
-                name="uf"
-                render={({ field }) => (
-                  <FormItem className="relative">
-                    <FormControl>
-                      <Input
-                        type="text"
-                        className="peer block w-16"
-                        placeholder=" "
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormLabel className="absolute left-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-zinc-300 text-sm text-zinc-600 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-yellow-800">
-                      UF
-                    </FormLabel>
-                  </FormItem>
+              {form.formState.errors.street && (
+                <span className="text-[0.8rem] font-medium text-red-500">
+                  {form.formState.errors.street.message}
+                </span>
+              )}
+            </div>
+
+            <div className="flex items-center gap-3">
+              <div className="flex flex-col gap-2">
+                <FormField
+                  control={form.control}
+                  name="number"
+                  render={({ field }) => (
+                    <FormItem className="relative">
+                      <FormControl>
+                        <Input
+                          type="text"
+                          className="peer block w-52"
+                          placeholder=" "
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormLabel className="absolute left-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-zinc-300 text-sm text-zinc-600 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-yellow-800">
+                        Número
+                      </FormLabel>
+                    </FormItem>
+                  )}
+                />
+                {form.formState.errors.number && (
+                  <span className="text-[0.8rem] font-medium text-red-500">
+                    {form.formState.errors.number.message}
+                  </span>
                 )}
-              />
+              </div>
+
+              <div className="w-full">
+                <FormField
+                  control={form.control}
+                  name="complement"
+                  render={({ field }) => (
+                    <FormItem className="relative">
+                      <FormControl>
+                        <Input
+                          type="text"
+                          className="peer block placeholder:text-xs placeholder:italic"
+                          placeholder="Opcional"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormLabel className="absolute left-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-zinc-300 text-sm text-zinc-600 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-yellow-800">
+                        Complemento
+                      </FormLabel>
+                    </FormItem>
+                  )}
+                />
+                {form.formState.errors.number && <div className="mb-5"></div>}
+              </div>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <div className="flex flex-col gap-2">
+                <FormField
+                  control={form.control}
+                  name="neighborhood"
+                  render={({ field }) => (
+                    <FormItem className="relative">
+                      <FormControl>
+                        <Input
+                          type="text"
+                          className="peer block w-52"
+                          placeholder=" "
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormLabel className="absolute left-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-zinc-300 text-sm text-zinc-600 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-yellow-800">
+                        Bairro
+                      </FormLabel>
+                    </FormItem>
+                  )}
+                />
+                {form.formState.errors.neighborhood && (
+                  <span className="text-[0.8rem] font-medium text-red-500">
+                    {form.formState.errors.neighborhood.message}
+                  </span>
+                )}
+              </div>
+
+              <div className="flex w-full flex-col gap-2">
+                <FormField
+                  control={form.control}
+                  name="city"
+                  render={({ field }) => (
+                    <FormItem className="relative">
+                      <FormControl>
+                        <Input
+                          type="text"
+                          className="peer block"
+                          placeholder=" "
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormLabel className="absolute left-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-zinc-300 text-sm text-zinc-600 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-yellow-800">
+                        Cidade
+                      </FormLabel>
+                    </FormItem>
+                  )}
+                />
+                {form.formState.errors.city && (
+                  <span className="text-[0.8rem] font-medium text-red-500">
+                    {form.formState.errors.city.message}
+                  </span>
+                )}
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <FormField
+                  control={form.control}
+                  name="uf"
+                  render={({ field }) => (
+                    <FormItem className="relative">
+                      <FormControl>
+                        <Input
+                          type="text"
+                          className="peer block w-16"
+                          placeholder=" "
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormLabel className="absolute left-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-zinc-300 text-sm text-zinc-600 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-yellow-800">
+                        UF
+                      </FormLabel>
+                    </FormItem>
+                  )}
+                />
+                {form.formState.errors.uf && (
+                  <span className="text-[0.8rem] font-medium text-red-500">
+                    {form.formState.errors.uf.message}
+                  </span>
+                )}
+              </div>
             </div>
           </div>
         </div>
