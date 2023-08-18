3692b7589946055c93944e8e32acf04d0cde792f
felipesanderp <felipe.panisson@hotmail.com>
2023-08-18 13:37:12 -0300

    drawer with shadcn ui

--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -1,29 +1,38 @@
-interface Props {
-  isOpen: boolean
-  children: React.ReactNode
-  onCartIconClick: () => void
-}
+'use client'
 
-export const Drawer = ({ children, isOpen, onCartIconClick }: Props) => {
+import useFromStore from '@/hooks/useFromState'
+import { useCartStore } from '@/stores/useCartStore'
+
+import { Cart } from '@/components/cart/cart'
+import { Icons } from '@/components/icons'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+
+// interface Props {
+//   // isOpen: boolean
+//   children: React.ReactNode
+//   // onCartIconClick: () => void
+// }
+
+export const Drawer = () => {
+  const cart = useFromStore(useCartStore, (state) => state.cart)
+
   return (
-    <div className="relative">
-      <div
-        className={`fixed right-0 top-0 z-50 h-full w-[30rem] transform bg-zinc-100 text-zinc-800 transition duration-700 ease-in-out ${
-          isOpen ? 'translate-x-0' : 'translate-x-full'
-        }`}
-        style={{
-          boxShadow: `${isOpen ? 'rgba(0, 0, 0, 0.4) 0px 30px 30px' : ''}`,
-        }}
-      >
-        <aside className="h-full overflow-y-auto">
-          <header className="flex h-14 items-center justify-end bg-zinc-100 px-4 py-4 text-zinc-800">
-            <div>
-              <button onClick={onCartIconClick}>Close</button>
-            </div>
-          </header>
-          <main className="p-4 text-zinc-800">{children}</main>
-        </aside>
-      </div>
-    </div>
+    <Sheet>
+      <SheetTrigger asChild>
+        <button className="flex items-center rounded-md bg-yellow-100 p-2 group-hover:bg-yellow-500">
+          <Icons.shoppingCart
+            width={22}
+            height={22}
+            className="fill-yellow-500 stroke-yellow-500 group-hover:fill-yellow-100 group-hover:stroke-yellow-100"
+          />
+          <div className="absolute -mt-9 ml-4 h-5 w-5 rounded-full bg-yellow-800 text-sm text-white">
+            {cart?.length}
+          </div>
+        </button>
+      </SheetTrigger>
+      <SheetContent className="w-[30rem]">
+        <Cart />
+      </SheetContent>
+    </Sheet>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,25 +1,13 @@
-'use client'
-
-import { useState } from 'react'
 import Image from 'next/image'
 
 import logo from '@/assets/logo.png'
-import { Icons } from './icons'
-import { Drawer } from './drawer'
-import useFromStore from '@/hooks/useFromState'
-import { useCartStore } from '@/stores/useCartStore'
-import { Cart } from './cart/cart'
+
+import { Icons } from '@/components/icons'
+import { Drawer } from '@/components/drawer'
+
 import Link from 'next/link'
 
 export function Header() {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
-
-  const cart = useFromStore(useCartStore, (state) => state.cart)
-
-  const handleCartIconClick = () => {
-    setIsDrawerOpen(!isDrawerOpen)
-  }
-
   return (
     <>
       <div className="h-104 container flex items-center justify-between p-8">
@@ -38,25 +26,10 @@ export function Header() {
             </button>
           </div>
           <div className="group relative">
-            <button
-              className="flex items-center rounded-md bg-yellow-100 p-2 group-hover:bg-yellow-500"
-              onClick={handleCartIconClick}
-            >
-              <Icons.shoppingCart
-                width={22}
-                height={22}
-                className="fill-yellow-500 stroke-yellow-500 group-hover:fill-yellow-100 group-hover:stroke-yellow-100"
-              />
-              <div className="absolute -mt-9 ml-4 h-5 w-5 rounded-full bg-yellow-800 text-sm text-white">
-                {cart?.length}
-              </div>
-            </button>
+            <Drawer />
           </div>
         </div>
       </div>
-      <Drawer isOpen={isDrawerOpen} onCartIconClick={handleCartIconClick}>
-        <Cart />
-      </Drawer>
     </>
   )
 }
